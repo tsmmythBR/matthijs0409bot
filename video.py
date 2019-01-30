19350d83f53ed483c7bee678cffb19650b6e7e84
tsmmythBR <47115775+tsmmythBR@users.noreply.github.com>
2019-01-30 18:46:01 +0100

    Create video.py

---
+++ video.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='SnorWare', type = 2))
+    print('All systems ready!') 
+
+
+@client.event
+async def on_message(message):
+    if message.content == '!help':
+        await client.send_message(message.channel,'Je kan de krant opvragen dmv !robloxdev of !snorkrant. Je kan je task opvragen dmv: !tasks (je naam)!')
+    if message.content == '!tasks':
+        await client.send_message(message.channel,'Je kan je task opvragen dmv: !tasks (je naam)!')
+    if message.content == '!tasks eDxiction':
+        await client.send_message(message.channel,'Je hebt op dit moment geen Task.')
+    if message.content == '!tasks demanmetsnor112':
+        await client.send_message(message.channel,'Je hebt op dit moment geen Task.')
+    if message.content == '!tasks matthijs0409':
+        await client.send_message(message.channel,'Je hebt op dit moment geen Task.')
+    if message.content == '!tasks dylanhaen':
+        await client.send_message(message.channel,'Je hebt op dit moment geen Task.')
+    if message.content == '!tasks jespower':
+        await client.send_message(message.channel,'Je hebt op dit moment geen Task.')
+    if message.content == '!robloxdev':
+        em = discord.Embed(description='Hier kun je alle kranten downloaden! https://mega.nz/#F!HfRBGQYD!sQWl7IemfeDFbywTVEiw3w')
+        em.set_image(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIL0Shg5fh-2rxGimUmuZKNz3QAZSs0T2Df9W7G6QnwV6n150U')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '!snorkrant':
+        em = discord.Embed(description='Hier kun je alle kranten downloaden! https://mega.nz/#F!zeIlwSxL!AkEGnpAzPZuyqdH-N-H2Rw')
+        em.set_image(url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIL0Shg5fh-2rxGimUmuZKNz3QAZSs0T2Df9W7G6QnwV6n150U')
+        await client.send_message(message.channel, embed=em)
